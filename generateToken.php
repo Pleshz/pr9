e231846b0b38f45c3f022604207f44bbc685c625
Pleshz <pleskovd96@gmail.com>
2026-03-01 11:14:26 +0500

    Добавил подключение к БД и реализовал проверку логина/пароля на доп домене. Плешков

--- generateToken.php
+++ generateToken.php
@@ -1,21 +1,31 @@
 <?php
+    include("connect_database.php");
+
     $SECRET_KEY = "Asdfg123";
 
-    $header = json_encode(['alg' => 'HS256', 'typ' => 'JWT']);
+    $login = $_POST["login"];
+    $password = $_POST["password"];
 
-    $payload = json_encode(['userId'=> 8,'roll'=> 0,'exp' => time() + 3600]);
+    $query = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."'");
 
-    function base64UrlEncode($data) {
-        return str_replace(['+','/','='],['-','_',''], base64_encode($data));
+    if($user = $query->fetch_assoc()) {
+        if($user['password'] == $password) {
+            $header = json_encode(['alg' => 'HS256', 'typ' => 'JWT']);
+            $payload = json_encode(['userId'=> $user['id'],'roll'=> $user['roll'],'exp' => time() + 3600]);
+            function base64UrlEncode($data) {
+                return str_replace(['+','/','='],['-','_',''], base64_encode($data));
+            }
+
+            $base64UrlHeader = base64UrlEncode($header);
+            $base64UrlPayload = base64UrlEncode($payload);
+
+            $signature = hash_hmac('sha256',$base64UrlHeader . "." . $base64UrlPayload, $SECRET_KEY, true);
+            $base64UrlSignature = base64UrlEncode($signature);
+
+            $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+
+            echo $jwt;
+            exit;
+        }
     }
-
-    $base64UrlHeader = base64UrlEncode($header);
-    $base64UrlPayload = base64UrlEncode($payload);
-
-    $signature = hash_hmac('sha256',$base64UrlHeader . "." . $base64UrlPayload, $SECRET_KEY, true);
-    $base64UrlSignature = base64UrlEncode($signature);
-
-    $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
-
-    echo $jwt;
 ?>
