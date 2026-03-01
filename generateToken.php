24f1019513a8f74bf5dbf5854feae5ccd710287c
Pleshz <pleskovd96@gmail.com>
2026-03-01 11:02:35 +0500

    Код генерации токена. Плешков

--- generateToken.php
+++ generateToken.php
@@ -1,3 +1,21 @@
 <?php
-    
+    $SECRET_KEY = "Asdfg123";
+
+    $header = json_encode(['alg' => 'HS256', 'typ' => 'JWT']);
+
+    $payload = json_encode(['userId'=> 8,'roll'=> 0,'exp' => time() + 3600]);
+
+    function base64UrlEncode($data) {
+        return str_replace(['+','/','='],['-','_',''], base64_encode($data));
+    }
+
+    $base64UrlHeader = base64UrlEncode($header);
+    $base64UrlPayload = base64UrlEncode($payload);
+
+    $signature = hash_hmac('sha256',$base64UrlHeader . "." . $base64UrlPayload, $SECRET_KEY, true);
+    $base64UrlSignature = base64UrlEncode($signature);
+
+    $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+
+    echo $jwt;
 ?>
